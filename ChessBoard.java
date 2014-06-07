87241f68ab947e7bd9f66b5f542cdffaf6c1f860
Patrick Tsai <patosai@live.com>
2014-06-07 16:06:11 -0400

    Attempted more checkmate fixes

--- ChessBoard.java
+++ ChessBoard.java
@@ -393,7 +393,7 @@ public class ChessBoard {
 				for (int i = 0; i < possMoves.size(); i++) {
 					ChessPiece temp = board[possMoves.get(i) / 10][possMoves.get(i) % 10];
 					movePiece(r, c, possMoves.get(i) / 10, possMoves.get(i) % 10);
-					if (!whiteToMove) 
+					if (whiteToMove) 
 						if (!wKing.amIInCheck(board)) {
 							movePiece(possMoves.get(i) / 10, possMoves.get(i) % 10, r, c);
 							board[possMoves.get(i) / 10][possMoves.get(i) % 10] = temp;
